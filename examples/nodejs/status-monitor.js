e3e4956bb4766a0a3e83629cc17f886f6a76a857
Matthew Carter <m@ahungry.com>
2019-08-05 01:42:39 -0400

    more tinkering

--- examples/nodejs/status-monitor.js
+++ examples/nodejs/status-monitor.js
@@ -17,12 +17,16 @@ const repeat = (n, s) => {
 function nlPad (s) {
   return repeat(10 - y, '\n') + s
 }
+function rlPad (s) {
+  return repeat(10 - x, ' ') + s
+}
 
 function send (m) {
   const port = 12345
   const host = '127.0.0.1'
   let payload = JSON.stringify(m)
   payload = nlPad(payload)
+  payload = rlPad(payload)
   console.log('Sending over: ', { payload })
   server.send(payload, 0, payload.length, port, host, callback);
 }
@@ -31,7 +35,8 @@ server.on('listening', function () {
   console.log('we are listening....:', server.address())
 })
 
-var y = 1
+var y = 0
+var x = 0
 
 const escape = () => String.fromCharCode(0x1B)
 const term = () => escape() + '['
@@ -40,13 +45,25 @@ const term = () => escape() + '['
 const cursor_up = () => {
   y = y + 1
 
-  return term() + '0;' + y + 'H ' + y
+  return x + ', ' + y
 }
 
 const cursor_down = () => {
   y = y - 1
 
-  return term() + '0;' + y + 'H ' + y
+  return x + ', ' + y
+}
+
+const cursor_left = () => {
+  x = x + 1
+
+  return x + ', ' + y
+}
+
+const cursor_right = () => {
+  x = x - 1
+
+  return x + ', ' + y
 }
 
 // Each time we get a message, lets send one back.
@@ -57,6 +74,8 @@ server.on('message', function (message, remote) {
     console.log({ message, remote })
     if (0xEA === message[0]) message = cursor_up() + 'UP_ARROW'
     if (0xEB === message[0]) message = cursor_down() + 'DOWN_ARROW'
+    if (0xE8 === message[0]) message = cursor_left() + 'LEFT_ARROW'
+    if (0xE9 === message[0]) message = cursor_right() + 'RIGHT_ARROW'
     send("Greetings from js  - you pushed: " + message)
   }
   // send("Did you just push: " + message)
